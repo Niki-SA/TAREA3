819b4f71ef6a71bc8c3d565e3693c860d7fda9c9
Niki <nicole.serrano2405@hotmail.com>
2024-09-08 21:07:36 -0600

    agregue juego

---
+++ juego.py
@@ -0,0 +1,19 @@
+import random
+
+def adivina_el_numero():
+    numero_secreto = random.randint(1, 100)
+    intentos = 0
+
+    while True:
+        intento = int(input("Adivina el número (entre 1 y 100): "))
+        intentos += 1
+
+        if intento < numero_secreto:
+            print("Demasiado bajo")
+        elif intento > numero_secreto:
+            print("Demasiado alto")
+        else:
+            print(f"¡Felicidades! Adivinaste el número en {intentos} intentos.")
+            break
+
+adivina_el_numero()
